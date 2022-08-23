a1c7f2ca9e439aaabb6a366a6256a5aeaa4a21dc
augusito <kaugustus15@gmail.com>
2022-08-23 18:00:27 +0300

    Add binding for middleware

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-import { NextFunction, Request, Response } from 'express';
 import { Application, MiddlewareFactory } from './core/application';
 import { HttpAdapter } from './core/http';
 import { LogFactory } from './core/logging';
@@ -12,22 +11,11 @@ class AppService {
 class AppHandler {
   constructor(private readonly appService: AppService) {}
 
-  handle(req: Request, res: Response) {
+  handle(req: any, res: any) {
     const { name = 'World' } = req.query;
     res.send(this.appService.sayHello(name as string));
   }
 }
-class Middleware {
-  process(req: Request, res: Response, next: NextFunction) {
-    const { name = 'World' } = req.query;
-    res.send(`Hello ${name}!`);
-  }
-}
-
-const fnMiddleware = (req: Request, res: Response) => {
-  const { name = 'There' } = req.query;
-  res.send(`Hello ${name}!`);
-};
 
 const factory = new MiddlewareFactory();
 const middleware = factory.prepare(new AppHandler(new AppService()));
@@ -35,10 +23,7 @@ const middleware = factory.prepare(new AppHandler(new AppService()));
 (async () => {
   const logger = LogFactory.getLog(Application.name);
   const app = new Application(new HttpAdapter(), { cors: true });
-  app.get('/', [
-    (req: Request, res: Response, next: NextFunction) =>
-      middleware.process(req, res, next),
-  ]);
+  app.get('/', middleware.process.bind(middleware));
 
   await app.listen(3000);
   logger.info(`Application started on: ${await app.getUrl()}`);
