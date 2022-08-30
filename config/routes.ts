78af74916c01bfd820b82806897c992878d65578
Augustus Kamau <kaugustus15@gmail.com>
2022-08-30 17:29:21 +0300

    Fix order module import

--- config/routes.ts
+++ config/routes.ts
@@ -1,13 +1,13 @@
 import { UserModule } from '../src/user/user.module';
 import { Application } from '../src/core/application';
 import { AppModule } from '../src/app/app.module';
-import { OrdersModule } from 'src/order/orders.module';
+import { OrderModule } from 'src/order/order.module';
 
 export default (app: Application) => {
   // App routes
   AppModule.registerRoutes(app);
   // Order routes
-  OrdersModule.registerRoutes(app);
+  OrderModule.registerRoutes(app);
   // User routes
   UserModule.registerRoutes(app);
 };
