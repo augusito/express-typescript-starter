5e5131577c201a1ae2aca5dbaddb9512258ebc2e
Augustus Kamau <kaugustus15@gmail.com>
2022-09-15 12:32:47 +0300

    Refactor in-memory container

--- lib/scheduler/__tests__/cron.orchestrator.test.ts
+++ lib/scheduler/__tests__/cron.orchestrator.test.ts
@@ -1,32 +1,9 @@
-import { IContainer, ProviderToken } from '../../container';
 import { SchedulerContainer } from '../scheduler-container';
 import { SchedulerFactory } from '../scheduler-factory';
 import { SchedulerType } from '../scheduler-type.enum';
 import { SchedulerOrchestrator } from '../scheduler.orchestrator';
 import { SchedulerRegistry } from '../scheduler.registry';
-
-class InMemoryContainer implements IContainer {
-  private readonly services = new Map<ProviderToken, any>();
-
-  get<T>(token: ProviderToken): T {
-    if (!this.has(token)) {
-      throw new Error(`${token.toString()} was not found`);
-    }
-    return this.services.get(token);
-  }
-
-  has(token: ProviderToken): boolean {
-    return this.services.has(token);
-  }
-
-  set(token: ProviderToken, item: any): void {
-    this.services.set(token, item);
-  }
-
-  reset(): void {
-    this.services.clear();
-  }
-}
+import { InMemoryContainer } from './in-memory-container';
 
 class CronOne {
   callsCount = 0;
