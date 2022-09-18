4c9d7991ce54387899521a1e80558686b6c2b09d
augusito <kaugustus15@gmail.com>
2022-09-18 09:05:11 +0300

    Refactor error generator types

--- lib/application/interfaces/error-generator.interface.ts
+++ lib/application/interfaces/error-generator.interface.ts
@@ -1,8 +1,9 @@
-export interface ErrorGenerator<T = any> {
+export interface ErrorGenerator<TError, TResponse> {
   /**
    * Method to implement a custom error generator.
    *
    * @param error the class of the error being handled
+   *  @param response the in-flight `response` object
    */
-  next(error: T, response: any): any;
+  next(error: TError, response: TResponse): any;
 }
